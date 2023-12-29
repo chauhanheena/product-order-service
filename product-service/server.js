8d3cf0780600344a11798f678028aa5dd20b37fc
Heena <chauhanheena020@gmail.com>
2023-12-29 13:15:48 +0530

    ENV variable use

--- product-service/server.js
+++ product-service/server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var axios = require('axios');
 var app = express();
 const port = 3000;
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+app.listen(port, () => console.log(`Application Running!`))
+const orderApiUrl = process.env.ORDER_API_URL;
 
 app.get('/products', function (req, res) {
     res.writeHead(200, {'Content-Type': 'application/json'});
@@ -10,3 +12,18 @@ app.get('/products', function (req, res) {
     console.log(products);
     res.end(JSON.stringify(products));
  })
+
+app.get('/productdetails/:product_name', function(req, res){
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    console.log('OrderBaseURL', orderApiUrl);
+    axios.get(orderApiUrl + '/orders/' + req.params.product_name)
+    .then(response => {
+        // Handle the API response
+        console.log('API Response:', response.data);
+        res.end(JSON.stringify(response.data));
+    })
+    .catch(error => {
+        // Handle errors
+        console.error('Error:', error.message);
+    });
+})
